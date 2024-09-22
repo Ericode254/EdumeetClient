731f1beb54484c5b25d5c3b59201267ff91ec596
Ericode254 <jilloerick6@gmail.com>
2024-09-22 20:49:54 +0300

    worked on the ui card for rendering the events from the server

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,29 @@
 import { Button } from "./ui/button"
 
-const Card = () => {
+interface Props {
+    title: string,
+    description: string,
+    image: string,
+    startTime: string,
+    endTime: string,
+    speaker: string
+}
+
+const Card = ({ title, description, image, startTime, endTime, speaker } : Props) => {
     return (
         <div className="flex-none h-50 card bg-base-100 w-96 shadow-xl">
             <figure>
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    src={`http://localhost:3000/uploads/${image}`}
                     alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title text-yellow-400">Shoes!</h2>
-                <p className="text-white">If a dog chews shoes whose shoes does he choose?</p>
+                <h2 className="card-title text-yellow-400">{`${title}`}</h2>
+                <p className="text-white">{`${description}`}</p>
+                <p className="text-white">{`${startTime}`}</p>
+                <p className="text-white">{`${endTime}`}</p>
                 <div className="card-actions justify-end">
-                    <Button className="textwhite bg-indigo-500">Register</Button>
+                    <Button className="textwhite bg-indigo-500">{`${speaker}`}</Button>
                 </div>
             </div>
         </div>
